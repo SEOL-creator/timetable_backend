b76bdae269d203ae6c61f06990256505705494ab
SEOL-creator <78312877+SEOL-creator@users.noreply.github.com>
2021-11-16 13:16:31 +0900

    Add register code

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -23,10 +23,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         write_only=True, required=True, validators=[validate_password]
     )
     password2 = serializers.CharField(write_only=True, required=True)
+    code = serializers.CharField(required=False, write_only=True, allow_blank=True)
+    is_active = serializers.BooleanField(required=False, default=False)
 
     class Meta:
         model = User
-        fields = ("email", "nickname", "password", "password2")
+        fields = ("email", "nickname", "password", "password2", "code", "is_active")
 
     def validate(self, attrs):
         if attrs["password"] != attrs["password2"]:
@@ -39,6 +41,8 @@ class RegisterSerializer(serializers.ModelSerializer):
             email=validated_data["email"], nickname=validated_data["nickname"]
         )
         user.set_password(validated_data["password"])
+        if validated_data["code"] == "j6sy5w":
+            user.is_active = True
         user.save()
 
         return user
