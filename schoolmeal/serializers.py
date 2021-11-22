9b9aa4ca7647eab5859e8274941efb8751d2ce11
SEOL-creator <78312877+SEOL-creator@users.noreply.github.com>
2021-11-22 13:38:26 +0900

    Change schoolmeal order

--- schoolmeal/serializers.py
+++ schoolmeal/serializers.py
@@ -6,7 +6,7 @@ class MealItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MealItem
         fields = ("name", "allergy_codes")
-        ordering = ("-id",)
+        ordering = ("id",)
 
 
 class MealSerializer(serializers.ModelSerializer):
